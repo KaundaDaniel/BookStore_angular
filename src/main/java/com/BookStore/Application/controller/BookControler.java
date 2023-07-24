7cc94e67c4ed4b5ab9c7a6bd45f32fa0e6fd0eff
KaundaDaniel <dannydagmaisutic@gmail.com>
2023-07-24 19:13:40 +0100

    Last update

--- src/main/java/com/BookStore/Application/controller/BookControler.java
+++ src/main/java/com/BookStore/Application/controller/BookControler.java
@@ -1,4 +1,11 @@
 package com.BookStore.Application.controller;
 
+import com.BookStore.Application.service.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/books")
 public class BookControler {
+    private BookService bookService ;
 }
