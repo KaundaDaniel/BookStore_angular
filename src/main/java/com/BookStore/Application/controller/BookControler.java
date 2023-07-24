f65f6038b2323180021b598f16d62285afda747a
KaundaDaniel <dannydagmaisutic@gmail.com>
2023-07-24 20:26:29 +0100

    get book by id

--- src/main/java/com/BookStore/Application/controller/BookControler.java
+++ src/main/java/com/BookStore/Application/controller/BookControler.java
@@ -1,11 +1,23 @@
 package com.BookStore.Application.controller;
 
+import com.BookStore.Application.model.Book;
 import com.BookStore.Application.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/books")
 public class BookControler {
+    @Autowired
+
     private BookService bookService ;
+    @GetMapping("{id}")
+    public ResponseEntity<Book> findById(@PathVariable Long id){
+        Book book= bookService.findById(id);
+        return ResponseEntity.ok().body(book);
+    }
 }
